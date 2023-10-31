f664b0ec5e2c445f9ce5fc0d71f8fef9024d917b
Krišjānis Ivbulis <ivbulisk@gmail.com>
2023-10-31 13:24:27 +0200

    Mv MainMenu script inside object

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -1,22 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 
 public class MainMenu : MonoBehaviour
 {
-     // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void PlayButton()
