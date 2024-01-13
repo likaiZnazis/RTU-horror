43564a98280b355377f356f8f988faa81f4ece5a
Krišjānis Ivbulis <65708516+AEF1551s@users.noreply.github.com>
2024-01-13 20:14:37 +0200

    Reformat AktivatoraPunkts

--- Assets/Interactivity/Scripts/Interactor.cs
+++ Assets/Interactivity/Scripts/Interactor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Interactor : MonoBehaviour
 {
     [SerializeField] private Transform _interactionPoint;
-    [SerializeField] private float _interactionPointRadius = 0.5f;
+    [SerializeField] private float _interactionPointRadius;
     [SerializeField] private LayerMask _interactableMask;
 
     private readonly Collider[] _colliders = new Collider[3];
