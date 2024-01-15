5e1a77f330f1f8bb7bdb0744ca0009c972fe3e08
TheAivis <aiviskuzmenkov@gmail.com>
2024-01-15 16:41:34 +0200

    HotFix - fast doorsmall fix
    Map:
    -DoorSmall = working
    -Chirp chirp sounds

--- Assets/Interactivity/Scripts/DoorSmall.cs
+++ Assets/Interactivity/Scripts/DoorSmall.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour, IInteractable
+public class DoorSmall : MonoBehaviour, IInteractable
 {
     [SerializeField] private string _promt;
     private Animator _animator;
     private bool _isOpen = false;
     public string InteractionPromt => _promt;
 
-   private void Start()
+    [SerializeField] private AudioSource doorOpenAudioSource;
+    [SerializeField] private float openDelay = 0;
+    [Space(10)]
+    [SerializeField] private AudioSource doorCloseAudioSource;
+    [SerializeField] private float closeDelay = 0.8f;
+
+    private void Start()
     {
         //Iegūst animācijas komponenti
         _animator = GetComponent<Animator>();
@@ -23,10 +29,15 @@ public class Door : MonoBehaviour, IInteractable
         if(_isOpen){
             _animator.Play("Opening 1");
             Debug.Log(message:"Atver mazās durvis");
+            doorOpenAudioSource.PlayDelayed(openDelay);
+
         }
-        else{
+        else
+        {
 			_animator.Play("Closing 1");
             Debug.Log(message:"Aizver mazās durvis");
+            doorOpenAudioSource.PlayDelayed(closeDelay);
+
         }
 
         return true;
